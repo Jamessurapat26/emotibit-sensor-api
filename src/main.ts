b29a7392a10ec7d1a1221ccfae7845bfae4614a2
james surapat <james262600@gmail.com>
2025-06-05 13:37:46 +0700

    update port in main.ts

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,8 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application is running on: http://0.0.0.0:${port}`);
 }
 bootstrap();
